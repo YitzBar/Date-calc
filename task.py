a15aed7dccffa4e19d7a378ef44ba866aa0e26a9
YitzBar <107581271+YitzBar@users.noreply.github.com>
2022-06-21 20:52:19 +0300

    Update task.py

--- task.py
+++ task.py
@@ -7,6 +7,8 @@ special_month_length = {
     11 : 30,
 }
 
+shortest_month = min(special_month_length.values())
+
 def is_leap_year(x):
     if(x%400 ==0):
         return True
@@ -19,22 +21,37 @@ def get_month_length(x):
         return special_month_length[x]
     return 31
 
-def is_valid_date_str(str):
-    return True
-
-def calc_new_date(orig_date, number_of_days):
-    if(number_of_days==0):
-        return orig_date
-        
-    retval = orig_date
-    day = orig_date.day
-    month = orig_date.month
-    year = orig_date.year
+def calc_new_date(str_date, number_of_days):
+    #----- validate parameters -----
+    if(number_of_days<0):
+        print('Invalid namber of days')
+        return str_date
     
-    while True:
-        if(number_of_days>28):
-            day = day + 28
-            number_of_days = number_of_days - 28
+    orig_date = str_date.split('.')  
+    
+    if(len(orig_date)!=3):
+        print('Invalid date format, should be DD.MM.YYYY')
+        return str_date
+    
+    for x in orig_date:
+        if not x.isdigit():
+            print('Invalid date value, date should contain numbers only.')
+            return str_date
+    
+    day = int(orig_date[0])
+    month = int(orig_date[1])
+    year = int(orig_date[2])
+    
+    if(day<0 or day>31 or month<1 or month>12):
+        print('Invalid date value.')
+        return str_date
+    
+    retval = '{}.{}.{}'
+    
+    while (number_of_days!=0):
+        if(number_of_days > shortest_month):
+            day = day + shortest_month
+            number_of_days = number_of_days - shortest_month
         else:
             day = day + number_of_days
             number_of_days = 0
@@ -51,20 +68,5 @@ def calc_new_date(orig_date, number_of_days):
         if month >= 13:
             month = 1
             year = year + 1
-        #print(day,".",month,'.',year,' days to add: ',number_of_days)    
-        if number_of_days == 0:
-            retval = datetime.datetime(year, month, day)
-            return retval
     
-    
-
-date = datetime.datetime(2022, 12, 30)
-num_of_days = -30
-print(date.strftime("%d.%m.%y"))
-print(calc_new_date(date,num_of_days).strftime("%d.%m.%y"))
-
-
-
-    
-
-        
+    return retval.format(day, month, year)
